4d1ef326ecbd24ae7599015a9658574036626776
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-07 17:39:09 +0530

    initial commit

---
+++ fruits-swiftui/fruits_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruits_swiftuiApp.swift
+//  fruits-swiftui
+//
+//  Created by Kshitij Singh Chauhan on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct fruits_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
